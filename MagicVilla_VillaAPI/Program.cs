2eb4f1998027beb9943d1da520cf5c152fe4af7f
Alexandre Akira <alexandreakiraakao@gmail.com>
2023-02-12 00:04:11 -0300

    feat: cleanup custom logger

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,5 +1,3 @@
-using MagicVilla_VillaAPI.Logging;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,7 +17,7 @@ builder.Services.AddControllers(option => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ILogging, LoggingV2>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
